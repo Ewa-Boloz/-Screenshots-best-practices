c78e0e34dc24eea6ba1bd462e5b322074ae91fcb
Ewa-Boloz <92880229+Ewa-Boloz@users.noreply.github.com>
2022-05-05 20:52:21 +0200

    commit

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {32:-1,36:-1,51:-1,50:-1,53:-1,35:-1,22:53,52:-1,27:53};
+return {35:-1,52:-1,22:53,51:-1,36:-1,53:-1,32:-1,27:53,50:-1};
 });
