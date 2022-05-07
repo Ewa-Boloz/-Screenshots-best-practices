8f9ccc96b07719f8647c94fabfbdb453afb53c33
Ewa-Boloz <92880229+Ewa-Boloz@users.noreply.github.com>
2022-05-07 09:00:13 +0200

    commit

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220507080750';
+                args = '20220507081727';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
